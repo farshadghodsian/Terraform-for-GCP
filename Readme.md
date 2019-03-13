397ffe6f03c3e0fbf1a420c9eca483213ca54229
Farshad G <farshadgz@gmail.com>
2019-03-13 17:47:30 -0400

    Added a terraform file for using inside GCP Cloud Shell and updated Readme

--- Readme.md
+++ Readme.md
@@ -2,4 +2,30 @@
 
 The Sample gcp-simple-environment.tf file will provision two VPC custom networks (one in US-East1 region and one in Europe-West1 region) each with their own submetworks. It will also provision 2 VM instances, one in each subnetwork and create Firewall rules to allow for SSH and ICMP (ping) connections between the two over the external network. Lastly, Terraform will also output the external and internal IPs of both servers to the console.
 
-<strong>Note: </strong> If you are running the gcp-simple-environment.tf file from inside GCP's Cloud Shell you must comment out or remove the entire <strong>provider "google" {...} </strong>reference as it is not needed and will cause an error.
+<strong>Note: </strong> If you are running the Terraform deployment from from inside GCP's Cloud Shell use the cloud-shell/gcp-cloudshell-demoenvironment.tf file as it has the provider section removed as it is not needed and will cause an error if included.
+
+When running outside of cloud shell you should use the demo/gcp-simple-environment.tf file and will need a <strong>credentials.json</strong> file stored in the root directory of this repo. The Credentials file can be generated in the GCP console IAM section from any service account that has permission to provision VPC Networks, Subnetworks, and Compute Instances.
+
+
+# Running the Terraform Deployment
+
+1. Regardless of which one you pick (cloud-shell or demo) you will need to navigate into that folder and initialize Terraform so that it can download the required Google privider plugin
+<br/>
+<code>terraform init</code>
+
+2. Once it has been initialized you will notice that a new .terraform directory has been created. Leave this folder alone as terraform will manage it on it's own. Next run the terraform plan file to review the changes
+<br/>
+<code>terraform plan --out test.plan </code>
+
+This will also output the plan to a file. It is always good practice to run the apply command using a plan file in case the .tf file has been changed by someone else before you actually have applied to plan to your environment. Not using a plan file can lead to some unforseen consequences.
+
+3. Apply the Terraform plan to your environment in GCP. If not using a plan file Terraform will prompt you to review and confirm the details before executing the deployment
+<br/>
+<code>terraform apply test.plan </code>
+
+4. To tear down your environment after you are done with the test environment use the terraform destroy command
+<br/>
+<code>terraform destroy </code>
+
+
+There are many more features that terraform offers, but this should be enough to get you started on using Terraform. Have fun!
---
+++ cloud-shell/gcp-cloudshell-demo-environment.tf
@@ -0,0 +1,154 @@
+
+############################
+####    VPC Networks    ####
+############################
+
+resource "google_compute_subnetwork" "subnet-a" {
+  name          = "subnet-a"
+  ip_cidr_range = "10.5.4.0/24"
+  region        = "us-east1"
+  network       = "${google_compute_network.vpn-network-1.self_link}"
+  # secondary_ip_range {
+  #   range_name    = "tf-test-secondary-range-update1"
+  #   ip_cidr_range = "192.168.10.0/24"
+  # }
+}
+
+resource "google_compute_network" "vpn-network-1" {
+  name                    = "vpn-network-1"
+  auto_create_subnetworks = "false"
+  description = "Primary VPC Network in US-East Region"
+}
+
+resource "google_compute_subnetwork" "subnet-b" {
+  name          = "subnet-b"
+  ip_cidr_range = "10.1.3.0/24"
+  region        = "europe-west1"
+  network       = "${google_compute_network.vpn-network-2.self_link}"
+  # secondary_ip_range {
+  #   range_name    = "tf-test-secondary-range-update1"
+  #   ip_cidr_range = "192.168.10.0/24"
+  # }
+}
+
+resource "google_compute_network" "vpn-network-2" {
+  name                    = "vpn-network-2"
+  auto_create_subnetworks = "false"
+  description = "Alternate VPC Network in Europe-West Region"
+}
+
+##############################
+####    Firewall Rules    ####
+##############################
+
+resource "google_compute_firewall" "allow-icmp-ssh-network-1" {
+  name    = "allow-icmp-ssh-network-1"
+  network = "${google_compute_network.vpn-network-1.name}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  #source_tags = ["web"]
+}
+
+resource "google_compute_firewall" "allow-icmp-ssh-network-2" {
+  name    = "allow-icmp-ssh-network-2"
+  network = "${google_compute_network.vpn-network-2.name}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  #source_tags = ["web"]
+}
+
+#################################
+####    Compute Instances    ####
+#################################
+resource "google_compute_instance" "server-1" {
+    depends_on = ["google_compute_subnetwork.subnet-a"]
+    name = "server-1"
+    zone = "us-east1-b"
+    machine_type = "f1-micro"
+
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    subnetwork = "${google_compute_subnetwork.subnet-a.self_link}"
+    access_config = {
+    }
+  }
+  # service_account {
+  #   email = ""
+  #   scopes = ["compute-rw"]
+  # }
+}
+
+resource "google_compute_instance" "server-2" {
+    depends_on = ["google_compute_subnetwork.subnet-b"]
+    name = "server-2"
+    zone = "europe-west1-b"
+    machine_type = "f1-micro"
+
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    subnetwork = "${google_compute_subnetwork.subnet-b.self_link}"
+    access_config = {
+    }
+  }
+  # service_account {
+  #   email = ""
+  #   scopes = ["compute-rw"]
+  # }
+}
+
+
+#######################
+####    Outputs    ####
+#######################
+
+output "server-1_externalip" {
+  depends_on=["google_compute_instance.server-1"]
+  value = "${google_compute_instance.server-1.network_interface.0.access_config.0.nat_ip}"
+  description = "External IP to access server-1"
+}
+
+output "server-1_internalip" {
+  depends_on=["google_compute_instance.server-1"]
+  value = "${google_compute_instance.server-1.network_interface.0.network_ip}"
+  description = "Internal IP to access server-1"
+}
+
+output "server-2_externalip" {
+  depends_on=["google_compute_instance.server-2"]
+  value = "${google_compute_instance.server-2.network_interface.0.access_config.0.nat_ip}"
+  description = "External IP to access server-2"
+}
+
+output "server-2_internalip" {
+  depends_on=["google_compute_instance.server-2"]
+  value = "${google_compute_instance.server-2.network_interface.0.network_ip}"
+  description = "Internal IP to access server-2"
+}
